58539e9a2f4bd23b3a6ef03cd96ee0e97ec6ab5d
Evan Hashemi <ehashemi@usc.edu>
2020-09-07 18:25:37 -0700

    Recieved message from serverw/ tcp_client.cpp

--- ee250/lab03/tcp_client.cpp
+++ ee250/lab03/tcp_client.cpp
@@ -16,14 +16,14 @@ int main(int argc, char const *argv[])
 	char socket_read_buffer[1024];
 	
 	// TODO: Fill out the server ip and port
-	std::string server_ip = "";
-	std::string server_port = "";
+	std::string server_ip = "34.209.114.30";
+	std::string server_port = "5001";
 
 	int opt = 1;
 	int client_fd = -1;
 
 	// TODO: Create a TCP socket()
-
+	client_fd =  socket(AF_INET, SOCK_STREAM, 0);
 	// Enable reusing address and port
 	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) { 
 		return -1;
@@ -44,17 +44,21 @@ int main(int argc, char const *argv[])
 	getaddrinfo(server_ip.c_str(), server_port.c_str(), &hints, &server_addr);
 
 	// TODO: Connect() to the aws server (hint: you'll need to use server_addr)
-
+	connect(client_fd, server_addr->ai_addr,server_addr->ai_addrlen); //sizeof(server_addr)
 	// TODO: Retreive user input
-
+	int i,length;
+	for(i=0;i<argc;i++){
+	  length = strlen(argv[i]);
+	  write(client_fd, argv[i],length+1);
 	// TODO: Send() the user input to the aws server
 
 	// TODO: Recieve any messages from the aws server and print it here. Don't forget to make sure the string is null terminated!
-	int len = recv(client_fd, socket_read_buffer, sizeof(socket_read_buffer), 0);
-	socket_read_buffer[len] = '\0';
-	printf("%s\n", socket_read_buffer);
+	  int len = recv(client_fd, socket_read_buffer, sizeof(socket_read_buffer), 0);
+	  socket_read_buffer[len] = '\0';
+	  printf("%s\n", socket_read_buffer);
+	}
 	
 	// TODO: Close() the socket
-
+	close(client_fd);
 	return 0; 
 } 
